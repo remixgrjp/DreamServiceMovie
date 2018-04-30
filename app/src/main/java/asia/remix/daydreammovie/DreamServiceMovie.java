508d6669dec1ca16f940ed353bffeed69fb82891
remix <http+github.com@remix.asia>
2018-05-01 08:19:36 +0900

    ADD MediaPlayer.OnPreparedListener

--- app/src/main/java/asia/remix/daydreammovie/DreamServiceMovie.java
+++ app/src/main/java/asia/remix/daydreammovie/DreamServiceMovie.java
@@ -1,10 +1,10 @@
 package asia.remix.daydreammovie;
 
+import android.service.dreams.DreamService;
 import android.os.Bundle;
 import android.widget.VideoView;
 import android.media.MediaPlayer;
 import android.os.Environment;
-import android.service.dreams.DreamService;
 import java.io.File;
 import android.util.Log;
 import android.widget.Toast;
@@ -14,11 +14,22 @@ public class DreamServiceMovie extends DreamService{
 	private VideoView videoview;
 
 	/**
+	*	movie ready → start
+	*/
+	class MyVideoPreparedListener implements android.media.MediaPlayer.OnPreparedListener{
+		@Override
+		public void onPrepared( MediaPlayer mediaPlayer ){
+			Log.d( TAG, "●MyVideoPreparedListener.onPrepared()" );
+			videoview.start();
+		}
+	}
+
+	/**
 	*	movie finish → repeat
 	*/
 	class MyVideoEndListener implements android.media.MediaPlayer.OnCompletionListener{
 		@Override
-		public void onCompletion( MediaPlayer mediaPlayer  ){
+		public void onCompletion( MediaPlayer mediaPlayer ){
 			Log.d( TAG, "●MyVideoEndListener.onCompletion()" );
 			videoview.seekTo( 0 );
 			videoview.start();
@@ -40,13 +51,14 @@ public class DreamServiceMovie extends DreamService{
 		Log.d( TAG, "●onDreamingStarted()" );
 
 		videoview = (VideoView)findViewById( R.id.videoview );
+		videoview.setOnPreparedListener( new MyVideoPreparedListener() );
 		videoview.setOnCompletionListener( new MyVideoEndListener() );
 		File file = new File( Environment.getExternalStoragePublicDirectory( Environment.DIRECTORY_MOVIES ), "sample.mp4" );
 		if( file.exists() ){
 			videoview.setVideoPath( file.toString() );
 			videoview.start();
 		}else{
-			Toast.makeText( getApplicationContext(), "file not found.", Toast.LENGTH_LONG ).show();
+			Toast.makeText( getApplicationContext(), R.string.msgFileNotFound, Toast.LENGTH_LONG ).show();
 		}
 	}
 }
